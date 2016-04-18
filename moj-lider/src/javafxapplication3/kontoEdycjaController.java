6e01835fcf1de415fffb7a749f070a0dd4ee0241
michusk <skoramichal3@hotmail.com>
2016-04-18 22:21:24 +0200

    logowanie praca

---
+++ moj-lider/src/javafxapplication3/kontoEdycjaController.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication3;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author sony vaio
+ */
+public class kontoEdycjaController implements Initializable {
+
+    @FXML
+    private Button wstecz;
+    @FXML
+    private Button zapiszZmiany;
+    @FXML
+    private TextField telefon;
+    @FXML
+    private TextField adres;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField nazwisko;
+    @FXML
+    private TextField imie;
+    @FXML
+    private Button wyloguj;
+    @FXML
+    private PasswordField password;
+    
+    @FXML
+    private void wyloguj() throws IOException {
+        
+        Stage stage; 
+        Parent root;
+        stage = (Stage) wyloguj.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("logowanie.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void wstecz() throws IOException {
+        
+        Stage stage; 
+        Parent root;
+        stage = (Stage) wstecz.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("logowanie.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
